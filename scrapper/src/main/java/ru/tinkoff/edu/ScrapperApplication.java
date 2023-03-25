1082fd90f18db2c650763f55c68026ba625a0285
borrowsorrow <51077910+borrowsorrow@users.noreply.github.com>
2023-03-26 01:33:03 +0300

    practice 1 task 1

---
+++ scrapper/src/main/java/ru/tinkoff/edu/ScrapperApplication.java
@@ -0,0 +1,16 @@
+package ru.tinkoff.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import ru.tinkoff.edu.configuration.*;
+
+@SpringBootApplication
+@EnableConfigurationProperties(ApplicationConfig.class)
+public class ScrapperApplication {
+public static void main(String []args){
+        var ctx= SpringApplication.run(ScrapperApplication.class,args);
+        ApplicationConfig config=ctx.getBean(ApplicationConfig.class);
+        System.out.println(config);
+        }
+}
