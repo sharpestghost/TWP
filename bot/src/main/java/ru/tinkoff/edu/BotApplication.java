c15989ea661812d5176b19cd1d0d53b730850fc8
borrowsorrow <51077910+borrowsorrow@users.noreply.github.com>
2023-03-26 17:16:38 +0300

    minor fix

--- bot/src/main/java/ru/tinkoff/edu/BotApplication.java
+++ bot/src/main/java/ru/tinkoff/edu/BotApplication.java
@@ -3,14 +3,15 @@ package ru.tinkoff.edu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import ru.tinkoff.edu.java.bot.configuration.ApplicationConfig;
+import ru.tinkoff.edu.configuration.ApplicationConfig;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationConfig.class)
 public class BotApplication {
 public static void main(String []args){
         var ctx= SpringApplication.run(BotApplication.class,args);
-        ApplicationConfig config=ctx.getBean(ApplicationConfig.class);
+        ApplicationConfig config = ctx.getBean(ApplicationConfig.class);
         System.out.println(config);
+        ctx.close();
         }
 }
--- scrapper/src/main/java/ru/tinkoff/edu/ScrapperApplication.java
+++ scrapper/src/main/java/ru/tinkoff/edu/ScrapperApplication.java
@@ -12,5 +12,6 @@ public static void main(String []args){
         var ctx= SpringApplication.run(ScrapperApplication.class,args);
         ApplicationConfig config=ctx.getBean(ApplicationConfig.class);
         System.out.println(config);
+        ctx.close();
         }
 }
