0f1c9064e147b9bb3af135f7499505cce0d7ecf0
borrowsorrow <51077910+borrowsorrow@users.noreply.github.com>
2023-03-27 09:52:54 +0300

    practice 3 task 4?

--- scrapper/src/main/java/ru/tinkoff/edu/ScrapperApplication.java
+++ scrapper/src/main/java/ru/tinkoff/edu/ScrapperApplication.java
@@ -3,9 +3,11 @@ package ru.tinkoff.edu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import ru.tinkoff.edu.configuration.ApplicationConfig;
 
 @SpringBootApplication
+@EnableScheduling
 @EnableConfigurationProperties(ApplicationConfig.class)
 public class ScrapperApplication {
     public static void main(String[] args) {
--- scrapper/src/main/java/ru/tinkoff/edu/configuration/ClientConfiguration.java
+++ scrapper/src/main/java/ru/tinkoff/edu/configuration/ClientConfiguration.java
@@ -1,12 +1,22 @@
 package ru.tinkoff.edu.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import reactor.core.Disposable;
+import reactor.core.scheduler.Scheduler;
 import ru.tinkoff.edu.GitHubClient;
 import ru.tinkoff.edu.StackOverflowClient;
 
+import java.time.Duration;
+
 @Configuration
+@EnableScheduling
 public class ClientConfiguration {
+
+    @Value("${app.scheduler.interval}")
+    private Duration interval;
 
     @Bean
     public GitHubClient gitHubClient() {
@@ -16,5 +26,10 @@ public class ClientConfiguration {
     @Bean
     public StackOverflowClient stackOverflowClient() {
         return new StackOverflowClient();
+    }
+
+    @Bean
+    public String delay(Duration interval) {
+        return interval.toString();
     }
 }
