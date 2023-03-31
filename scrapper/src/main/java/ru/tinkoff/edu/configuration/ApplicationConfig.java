14848438f536033269491273a6af6a22f848406b
borrowsorrow <51077910+borrowsorrow@users.noreply.github.com>
2023-03-31 08:07:43 -0700

    practice 3 fixes: redundant annotations

--- scrapper/src/main/java/ru/tinkoff/edu/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/ru/tinkoff/edu/configuration/ApplicationConfig.java
@@ -2,13 +2,9 @@ package ru.tinkoff.edu.configuration;
 
 import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.validation.annotation.Validated;
 import reactor.core.scheduler.Scheduler;
 
-@Configuration
-@EnableScheduling
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 public record ApplicationConfig(@NotNull String test,  @NotNull Scheduler scheduler) {}
--- scrapper/src/main/java/ru/tinkoff/edu/configuration/ClientConfiguration.java
+++ scrapper/src/main/java/ru/tinkoff/edu/configuration/ClientConfiguration.java
@@ -3,16 +3,12 @@ package ru.tinkoff.edu.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import reactor.core.Disposable;
-import reactor.core.scheduler.Scheduler;
 import ru.tinkoff.edu.GitHubClient;
 import ru.tinkoff.edu.StackOverflowClient;
 
 import java.time.Duration;
 
 @Configuration
-@EnableScheduling
 public class ClientConfiguration {
 
     @Value("${app.scheduler.interval}")
