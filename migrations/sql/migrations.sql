1ffead21e97866554aa9e637da61900dd819fd46
borrowsorrow <51077910+borrowsorrow@users.noreply.github.com>
2023-04-30 23:06:52 +0300

    edit create.sql

--- migrations/sql/migrations.sql
+++ migrations/sql/migrations.sql
@@ -1,15 +1,14 @@
 create table if not exists Link (
-id bigint primary key,
+id int primary key,
 url varchar(255) unique not null,
 linkname varchar(255) not null,
-description text,
-updated_at timestamp default current_timestamp
+updated_at timestamp default current_timestamp,
+answer_count int
 );
 
 create table if not exists Chat (
 id int primary key,
 chatname varchar(255) not null,
-description text,
 created_at timestamp default current_timestamp,
 updated_at timestamp default current_timestamp
 );
@@ -18,6 +17,6 @@ create table if not exists Link_Chat (
 chat_id integer not null,
 link_id integer not null,
 primary key (chat_id, link_id),
-foreign key (chat_id) references Chat(id) on delete cascade,
-foreign key (link_id) references Link(id) on delete cascade
+foreign key (chat_id) references Chat(id) ON DELETE CASCADE,
+foreign key (link_id) references Link(id) ON DELETE CASCADE
 );
