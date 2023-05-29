a7cd207f39170046cb8d3c08a4845b9756ca8fe7
borrowsorrow <51077910+borrowsorrow@users.noreply.github.com>
2023-05-29 23:29:02 +0300

    pre-practice 10 last scrapper checkstyle fix

--- scrapper/src/main/java/ru/tinkoff/edu/entity/LinkChat.java
+++ scrapper/src/main/java/ru/tinkoff/edu/entity/LinkChat.java
@@ -15,6 +15,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class LinkChat implements Serializable {
+    @SuppressWarnings("checkstyle:MemberName")
     private Long chat_id;
+
+    @SuppressWarnings("checkstyle:MemberName")
     private Long link_id;
 }
